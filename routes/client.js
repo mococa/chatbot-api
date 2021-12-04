bf561112edf86402de38cd6ccd810f82f80a885b
mococa <xfazxtudo@gmail.com>
2021-12-04 14:41:36 +0000

    handled client errors

--- routes/client.js
+++ routes/client.js
@@ -7,19 +7,37 @@ import { ClientModel } from "../models/client";
 export const client_route = Router();
 const route = client_route;
 
+route.get("/", async (req, res) => {
+  const clients = await Client.get();
+  return res.json(clients);
+});
+
+route.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const clients = await Client.get(id);
+  return res.json(clients);
+});
 route.get("/:id/chat", async (req, res) => {
   const { id } = req.params;
-  const client = await Client.get(id);
-  const jid = phoneToJid(client.phone);
-  const chat = await WhatsappBot.getClient().loadMessages(jid, 20);
-  return res.json(chat);
+  const bot = WhatsappBot.getClient();
+  if (bot && bot.isConnected) {
+    const client = await Client.get(id);
+    const jid = phoneToJid(client.phone);
+    const chat = await bot.loadMessages(jid, 20);
+    return res.json(chat);
+  }
+  return res.status(404).json({ message: "O Bot está atualmente desligado" });
 });
 route.post("/:id/chat", async (req, res) => {
   const { id } = req.params;
-  const client = await Client.get(id);
-  const jid = phoneToJid(client.phone);
-  const message = req.body.message;
-  await WhatsappBot.getClient().sendMessage(jid, message, "conversation");
-  const chat = await WhatsappBot.getClient().loadMessages(jid, 20);
-  return res.json(chat);
+  const bot = WhatsappBot.getClient();
+  if (bot && bot.isConnected) {
+    const client = await Client.get(id);
+    const jid = phoneToJid(client.phone);
+    const message = req.body.message;
+    await bot.sendMessage(jid, message, "conversation");
+    const chat = await bot.loadMessages(jid, 20);
+    return res.json(chat);
+  }
+  return res.status(404).json({ message: "O Bot está atualmente desligado" });
 });
