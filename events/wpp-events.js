11dcda051ce2f50becce068f602d9908e85922e3
mococa <xfazxtudo@gmail.com>
2021-12-04 13:49:15 +0000

    add test numbers array

--- events/wpp-events.js
+++ events/wpp-events.js
@@ -7,7 +7,7 @@ import { Client } from "../controllers/client";
 import { ClientModel } from "../models/client";
 import { SettingsModel } from "../models/settings";
 let jid = null;
-const test_number = "21982869775";
+const test_numbers = ["21982869775", "21993593730"];
 // const test_message = "Oi";
 // const test_reply = "Olá";
 
@@ -58,88 +58,90 @@ export const handle_wpp_events = (connection, session) => {
       const data = chat_update.messages?.all()[0];
       jid = data?.key?.remoteJid;
       const message = data?.message?.conversation;
-
-      if (jid === phoneToJid(test_number)) {
-        const customer = jid;
-        const client = await Client.findByPhone(jidToPhone(jid));
-        const session = Chatbot.findSession(customer);
-        if (client) {
-          if (session) {
-            Chatbot.answerQuestion({ message, customer });
-            Chatbot.onSessionEnd(customer, async (session) => {
-              console.log({ session });
-              reply(JSON.stringify(session));
-            });
-          } else {
-            const questions = ["Pergunta 1:", "Pergunta 2"];
-            Chatbot.createSession({
-              customer,
-              allQuestions: questions,
-            });
-          }
-        } else {
-          if (session) {
-            Chatbot.answerQuestion({ message, customer });
-            Chatbot.onSessionEnd(customer, async (session) => {
-              const imgUrl = await connection.getProfilePicture(jid);
-              await ClientModel.create({
-                name: session.message[0],
-                email: session.message[1],
-                picture: imgUrl,
-                phone: jidToPhone(jid),
-                channels: ["whatsapp"],
-              });
-              reply("Seu usuário foi criado com sucesso");
-            });
-          } else {
-            const settings = await SettingsModel.findOne({});
-            const welcome = (settings.welcome?.length && settings.welcome) || [
-              "Digite seu nome:",
-              "Digite seu e-mail",
-            ];
-            Chatbot.createSession({
-              customer,
-              allQuestions: welcome,
-            });
-            const new_session = Chatbot.findSession(customer);
-            reply(new_session.allQuestions[0]);
-          }
-        }
-        // if (session && !client) {
-        //   Chatbot.answerQuestion({ message, customer });
-        //   Chatbot.onSessionEnd(customer, async (session) => {
-        //     const imgUrl = await connection.getProfilePicture(jid);
-        //     await ClientModel.create({
-        //       name: session.message[0],
-        //       age: session.message[1],
-        //       email: session.message[2],
-        //       picture: imgUrl,
-        //       phone: jidToPhone(jid),
-        //       channels: ["whatsapp"],
-        //     });
-        //     reply("Seu usuário foi criado com sucesso");
-        //   });
-        // } else {
-        //   let questions = [
-        //     "Bem vindo ao BOT da familia tradicional brasileira... Para começar, preciso que você indique o seu nome. to esperando, vai...",
-        //     "Uui!, que nome bonitinho... rsrsrsrsr... agora sua idade........",
-        //     "entendi! agora me diz seu email aí porra!",
-        //   ];
-        //   if (client)
-        //     questions = [
-        //       `Oi, ${client.name}, esse é vc?? ${imgUrl}`,
-        //       `Tudo bem? Eu sei que vc tem ${client.age} anos`,
-        //       `E aí, blz, ${client.email} ?`,
-        //     ];
-        //   if (!session)
-        //     Chatbot.createSession({
-        //       customer,
-        //       allQuestions: questions,
-        //     });
-        //   const new_session = Chatbot.findSession(customer);
-        //   reply(new_session.allQuestions[0]);
-        // }
+      if (!test_numbers.some((test_number) => jid.includes(test_number))) {
+        return;
       }
+      //if (jid === phoneToJid(test_number)) {
+      const customer = jid;
+      const client = await Client.findByPhone(jidToPhone(jid));
+      const session = Chatbot.findSession(customer);
+      if (client) {
+        if (session) {
+          Chatbot.answerQuestion({ message, customer });
+          Chatbot.onSessionEnd(customer, async (session) => {
+            console.log({ session });
+            reply(JSON.stringify(session));
+          });
+        } else {
+          const questions = ["Pergunta 1:", "Pergunta 2"];
+          Chatbot.createSession({
+            customer,
+            allQuestions: questions,
+          });
+        }
+      } else {
+        if (session) {
+          Chatbot.answerQuestion({ message, customer });
+          Chatbot.onSessionEnd(customer, async (session) => {
+            const imgUrl = await connection.getProfilePicture(jid);
+            await ClientModel.create({
+              name: session.message[0],
+              email: session.message[1],
+              picture: imgUrl,
+              phone: jidToPhone(jid),
+              channels: ["whatsapp"],
+            });
+            reply("Seu usuário foi criado com sucesso");
+          });
+        } else {
+          const settings = await SettingsModel.findOne({});
+          const welcome = (settings.welcome?.length && settings.welcome) || [
+            "Digite seu nome:",
+            "Digite seu e-mail",
+          ];
+          Chatbot.createSession({
+            customer,
+            allQuestions: welcome,
+          });
+          const new_session = Chatbot.findSession(customer);
+          reply(new_session.allQuestions[0]);
+        }
+      }
+      // if (session && !client) {
+      //   Chatbot.answerQuestion({ message, customer });
+      //   Chatbot.onSessionEnd(customer, async (session) => {
+      //     const imgUrl = await connection.getProfilePicture(jid);
+      //     await ClientModel.create({
+      //       name: session.message[0],
+      //       age: session.message[1],
+      //       email: session.message[2],
+      //       picture: imgUrl,
+      //       phone: jidToPhone(jid),
+      //       channels: ["whatsapp"],
+      //     });
+      //     reply("Seu usuário foi criado com sucesso");
+      //   });
+      // } else {
+      //   let questions = [
+      //     "Bem vindo ao BOT da familia tradicional brasileira... Para começar, preciso que você indique o seu nome. to esperando, vai...",
+      //     "Uui!, que nome bonitinho... rsrsrsrsr... agora sua idade........",
+      //     "entendi! agora me diz seu email aí porra!",
+      //   ];
+      //   if (client)
+      //     questions = [
+      //       `Oi, ${client.name}, esse é vc?? ${imgUrl}`,
+      //       `Tudo bem? Eu sei que vc tem ${client.age} anos`,
+      //       `E aí, blz, ${client.email} ?`,
+      //     ];
+      //   if (!session)
+      //     Chatbot.createSession({
+      //       customer,
+      //       allQuestions: questions,
+      //     });
+      //   const new_session = Chatbot.findSession(customer);
+      //   reply(new_session.allQuestions[0]);
+      // }
+      //}
       //console.log({ message, jid });
       //connection?.chatRead();
       // if (jid === `${test_number}@s.whatsapp.net`) {
